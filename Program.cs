aff47ed5b47ca7c24dd6b152700c9409582b0310
BPYM <pavlikgavr2008@ya.ru>
2023-05-19 15:51:13 +0300

    Моё первое занятие по C Sharp

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+int a = 6;
+int b = 9;
+int c = a * b;
+Console.WriteLine(c);
+*/
+Console.WriteLine("Введите ваше имя: ");
+string Username = Console.ReadLine();
+if (Username.ToLower() == "bebrik")
+{ 
+Console.WriteLine("Дарова, " + Username);
+}
+else
+{
+    Console.WriteLine("Здравствуйте, " + Username);
+}
+
